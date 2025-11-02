26537a42ee63a89c89bcb27276d8dfda5d473df8
darylxslr <daryllacorte2005@gmail.com>
2025-11-02 23:35:04 +0800

    feat: re-implemented the jwt-authentication asynch activity

--- config.py
+++ config.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey') # for JWT
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
         f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'uploads')
+    LOG_FOLDER = 'logs'
     DEBUG = True
--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request, jsonify
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
+from jose import jwt
+from config import Config  
 
 auth_bp = Blueprint('auth_bp', __name__)
 
-# static auth
+# auth static credentials
 VALID_USERNAME = "password"
 VALID_PASSWORD = "username"
 
@@ -22,11 +24,23 @@ def login():
 
         # Successful login
         if username == VALID_USERNAME and password == VALID_PASSWORD:
+            # valid for 30 minutes
+            token = jwt.encode({
+                'username': username,
+                'exp': datetime.utcnow() + timedelta(minutes=30)
+            }, Config.SECRET_KEY, algorithm='HS256')
+
             logging.info(f"{datetime.now()} [INFO] Login successful for user: {username}")
-            return jsonify({'message': 'Login successful'}), 200
-        else:
-            logging.warning(f"{datetime.now()} [WARNING] Login failed for user: {username}")
-            return jsonify({'message': 'Invalid credentials'}), 401
+            logging.info(f"{datetime.now()} [INFO] Token created for user: {username}")
+
+            return jsonify({
+                'message': 'Login successful',
+                'token': token
+            }), 200
+
+        # Invalid credentials
+        logging.warning(f"{datetime.now()} [WARNING] Login failed for user: {username}")
+        return jsonify({'message': 'Invalid credentials'}), 401
 
     except Exception as e:
         logging.error(f"{datetime.now()} [ERROR] Login error: {e}")
